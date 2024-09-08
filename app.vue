e25598db9e1efb19e5a79774b22fa893bd71e3d3
broisnischal <neeswebservice@gmail.com>
2024-09-08 21:57:42 +0545

    changes

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1>First website from AWS Amplify</h1>
-    <h2>Nuxt</h2>
+  <NuxtRouteAnnouncer />
+  <div class="min-h-screen flex-col gap-3 flex items-center justify-center">
+    <h1 class="text-4xl">ping@nischal.pro</h1>
+    <div class="flex gap-2">
+      <a href="https://nischal-dahal.com.np" class="underline">portfolio</a>
+      <a href="https://nischal-dahal.com.np/sponsor" target="_blank" class="underline">sponsor</a>  
+      <a href="https://nischal-dahal.com.np/links" target="_blank" class="underline">contact</a>  
+    </div>
   </div>
 </template>
